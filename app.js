422a5e080b15a4b3390abbe2cb0e0d57cffef473
Pablo Ruiz <pabloruiz@ctnaval.com>
2020-01-31 20:28:31 +0100

    añado URL_CORS al .env

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use((req, res, next) => {
 
 // middleware para aceptar peticiones de otra aplicación (distinto servidor normalmente, o sino, distinto puerto)
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3003"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", process.env.URL_CORS); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
